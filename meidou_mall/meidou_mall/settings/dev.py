44a80f8e17909f90e2ccd5c8345bdb8143591711
gxz <gxz987@qq.com>
2019-06-28 15:38:17 +0800

    MySQL数据库的配置

--- meidou_mall/meidou_mall/settings/dev.py
+++ meidou_mall/meidou_mall/settings/dev.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'meidou_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':3306,
+        'USER':'meiduo',
+        'PASSWORD':'meiduo',
+        'NAME': 'meiduo_mall',
     }
 }
 
