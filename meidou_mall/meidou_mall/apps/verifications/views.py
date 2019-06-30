85642f878f114efc362099ec34aa2b16442dcb57
gxz <gxz987@qq.com>
2019-06-30 12:57:30 +0800

    短信验证码后端接口设计,发送短信验证码序列化器实现

--- meidou_mall/meidou_mall/apps/verifications/views.py
+++ meidou_mall/meidou_mall/apps/verifications/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from meidou_mall.libs.captcha.captcha import captcha
 from django_redis import get_redis_connection
 
+from verifications.serializer import ImageCodeCheckSerializer
 from . import constants
 
 # Create your views here.
@@ -24,3 +26,22 @@ class ImageCodeView(APIView):
         # 固定返回验证码图片数据，不需要REST framework框架的Response帮助我们决定返回响应数据的格式
         # 所以此处直接使用Django原生的HttpResponse即可
         return HttpResponse(image, content_type="images/jpg")
+
+
+# url('^/sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
+class SMSCodeView(GenericAPIView):
+    """短信验证码"""
+    # 传入参数:mobile, image_code_id, text
+
+    serializer_class = ImageCodeCheckSerializer
+    def get(self):
+        # 校验参数, 由序列化器完成
+
+        # 生成短信验证码
+
+        # 保存短信验证码  发送记录
+
+        # 发送短信
+
+        # 返回数据
+        pass
