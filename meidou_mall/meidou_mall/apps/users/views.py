2bd93966183c69f2ff2b87041c940de5dab4f0dd
gxz <gxz987@qq.com>
2019-07-01 20:42:01 +0800

    注册时,检查用户名是否已存在

--- meidou_mall/meidou_mall/apps/users/views.py
+++ meidou_mall/meidou_mall/apps/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
+
+
+class UsernameCountView(APIView):
+    '''用户名数量,用于判断用户名是否已存在'''
+    # 因逻辑本身比较简单,用GenericAPIView的话还需要定义序列化器,故直接继承APIView
+    def get(self, request, username):
+        '''获取指定用数量'''
+        count = User.objects.filter(username=username).count()
+        data = {
+            'username': username,
+            'count': count
+        }
+        return Response(data)
