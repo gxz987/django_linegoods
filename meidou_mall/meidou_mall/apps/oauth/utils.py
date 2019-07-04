b8f258db2a3fad1a04546377df23f30e290c2172
gxz <gxz987@qq.com>
2019-07-04 20:40:57 +0800

    获取access_token的实现

--- meidou_mall/meidou_mall/apps/oauth/utils.py
+++ meidou_mall/meidou_mall/apps/oauth/utils.py
@@ -5,6 +5,7 @@ from django.conf import settings
 import json
 import logging
 
+from oauth.exceptions import OAuthQQAPIError
 
 logger = logging.getLogger('django')
 
@@ -13,9 +14,9 @@ class OAuthQQ(object):
     """QQ认证辅助工具类"""
     def __init__(self, client_id=None, client_secret=None, redirect_uri=None, state=None):
         self.client_id = client_id or settings.QQ_CLIENT_ID
-        # self.client_secret = client_secret or settings.QQ_CLIENT_SECRET
         self.redirect_uri = redirect_uri or settings.QQ_REDIRECT_URI
         self.state = state or settings.QQ_STATE  # 用于保存登录成功后的跳转页面路径
+        self.client_secret = client_secret or settings.QQ_CLIENT_SECRET
 
     def get_login_url(self):
         """
@@ -32,3 +33,31 @@ class OAuthQQ(object):
 
         url += urlencode(params)
         return url
+
+    def get_access_token(self, code):
+        """获取access_token"""
+        url = 'https://graph.qq.com/oauth2.0/token?'
+        params = {
+            'grant_type': 'authorization_code',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'code': code,
+            'redirect_uri': self.redirect_uri,
+        }
+        url += urlencode(params)
+
+        try:
+            # 发送请求
+            resp = urlopen(url)
+            # 读取响应体的数据
+            resp_data = resp.read()  # bytes
+            resp_data = resp_data.decode()  # str
+            # 解析access_token
+            resp_dict = parse_qs(resp_data)
+        except Exception as e:
+            logger.error('获取access_token异常:%s' % e)
+            raise OAuthQQAPIError
+        else:
+            access_token = resp_dict.get('access_token')
+
+            return access_token
--- meidou_mall/meidou_mall/apps/oauth/views.py
+++ meidou_mall/meidou_mall/apps/oauth/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from oauth.utils import OAuthQQ
-
+from .exceptions import OAuthQQAPIError
 
 #  url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
 class QQAuthURLView(APIView):
@@ -20,3 +21,19 @@ class QQAuthURLView(APIView):
         login_url = oauth_qq.get_login_url()
 
         return Response({'login_url': login_url})
+
+
+class QQAuthUserView(APIView):
+    """qq登录的用户"""
+    def get(self, request):
+        # 获取code
+        code = request.query_params.get('code')
+        if not code:
+            return Response({'message': '缺少code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 根据code 获取access_token
+        oauth_qq = OAuthQQ()
+        try:
+            access_token = oauth_qq.get_access_token(code)
+        except OAuthQQAPIError:
+            return Response()
