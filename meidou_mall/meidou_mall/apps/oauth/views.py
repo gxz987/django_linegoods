3f138d8935e09c58d62993e331e62c663d2dd532
gxz <gxz987@qq.com>
2019-07-04 19:31:42 +0800

    获取qq登录网址实现(qq登录第一步)

--- meidou_mall/meidou_mall/apps/oauth/views.py
+++ meidou_mall/meidou_mall/apps/oauth/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from oauth.utils import OAuthQQ
+
+
+#  url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
+class QQAuthURLView(APIView):
+    """获取qq登录的url"""
+    def get(self, request):
+        """提供用于qq登录的url"""
+        # 获取next参数
+        next = request.query_params.get('next')
+
+        # 拼接qq登录的网址
+        oauth_qq = OAuthQQ(state=next)
+        login_url = oauth_qq.get_login_url()
+
+        return Response({'login_url': login_url})
