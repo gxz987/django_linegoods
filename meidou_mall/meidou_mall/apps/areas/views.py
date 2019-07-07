06edf9a33061f1fd1599bc9e6b4dfac9f29cf8a8
gxz <gxz987@qq.com>
2019-07-07 19:20:43 +0800

    新增收货地址的后端实现

--- meidou_mall/meidou_mall/apps/areas/views.py
+++ meidou_mall/meidou_mall/apps/areas/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from areas.models import Area
+from . import serializers
+
+
+class AreaViewSet(ReadOnlyModelViewSet):
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return Area.objects.filter(parent=None)
+        else:
+            return Area.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.AreaSerializer
+        else:
+            return serializers.SubAreaSerializer
