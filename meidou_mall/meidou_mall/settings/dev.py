0f708cd9673cdb010fc4c42851370cb5b9295124
gxz <gxz987@qq.com>
2019-07-13 19:41:04 +0800

    购物车增加商品的实现

--- meidou_mall/meidou_mall/settings/dev.py
+++ meidou_mall/meidou_mall/settings/dev.py
@@ -177,7 +177,7 @@ CACHES = {
     },
     "cart": {  # 购物车
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/3",
+        "LOCATION": "redis://127.0.0.1:6379/4",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
