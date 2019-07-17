322c0d178c07ae178cc7d0e05f8cb317e26691b9
gxz <gxz987@qq.com>
2019-07-17 17:20:13 +0800

    获取支付宝支付链接

--- meidou_mall/meidou_mall/apps/payment/views.py
+++ meidou_mall/meidou_mall/apps/payment/views.py
@@ -1,3 +1,52 @@
+import os
+from alipay import AliPay
+from django.conf import settings
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+from orders.models import OrderInfo
+
+
+class PaymentView(APIView):
+    """
+    支付
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, order_id):
+        """
+        获取支付链接
+        """
+        # 判断订单信息是否正确
+        try:
+            order = OrderInfo.objects.get(order_id=order_id, user=request.user,
+                                          pay_method=OrderInfo.PAY_METHODS_ENUM["ALIPAY"],
+                                          status=OrderInfo.ORDER_STATUS_ENUM["UNPAID"])
+        except OrderInfo.DoesNotExist:
+            return Response({'message': '订单信息有误'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 构造支付宝支付链接地址
+        alipay_client = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/app_private_key.pem"),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/alipay_public_key.pem"),  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=settings.ALIPAY_DEBUG  # 默认False
+        )
+
+        # 电脑网站支付，需要跳转到https://openapi.alipay.com/gateway.do? + order_string
+        order_string = alipay_client.api_alipay_trade_page_pay(
+            out_trade_no=order_id,  # 订单编号
+            total_amount=str(order.total_amount),  # 总金额
+            subject="美多商城%s" % order_id,
+            return_url="http://www.meiduo.site:8080/pay_success.html",
+        )
+        # 需要跳转到https://openapi.alipay.com/gateway.do? + order_string
+        # 拼接链接返回前端
+        alipay_url = settings.ALIPAY_URL + "?" + order_string
+        return Response({'alipay_url': alipay_url})
